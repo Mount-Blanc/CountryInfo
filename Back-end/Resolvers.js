ad3a5e8c1e3621596e891289ddd7144b63d23606
Mount-Blanc <mountblanc87@gmail.com>
2023-01-21 15:25:11 -0800

    added the basic structure of the api request

--- Back-end/Resolvers.js
+++ Back-end/Resolvers.js
@@ -1,4 +1,16 @@
 
+import axios from 'axios';
+
+axios.get('https://countries.trevorblades.com')
+  .then( (response) => {
+    // handle success
+    console.log(response);
+  })
+  .catch((error) => {
+    // handle error
+    console.log(error);
+  });
+
 const Cars = [
     {
             Brand: 'Audio',
@@ -20,7 +32,7 @@ const books = [
     author: 'Paul Auster',
   },
 ];
-
+ const response = fetch('https://countries.trevorblades.com/')
 
 export const resolvers = {
   Query: {
