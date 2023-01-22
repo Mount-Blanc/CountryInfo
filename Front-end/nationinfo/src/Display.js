b8d445325d1dedbc19ee88063565cf6cfecc3d97
Mount-Blanc <mountblanc87@gmail.com>
2023-01-22 14:27:39 -0800

    Got the front -end to successfully recieve data from the back end :)

---
+++ Front-end/nationinfo/src/Display.js
@@ -0,0 +1,45 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_LOCATIONS = gql`
+
+  query GetLocations {
+
+    books {
+
+     title
+     author
+
+    }
+
+  }
+
+`;
+
+
+const Display = () => {
+    const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+let content
+if (loading) {
+return content = <p>Loading...</p>
+}
+    return data.books.map(({ title, author }) => (
+        <>
+        {error 
+            ? (<p>An error occured !</p>)
+            : (<div >
+    
+          <h3>{title}</h3>    
+          <br />
+        
+          <p>{author}</p>
+    
+          <br />
+    
+        </div>)}
+        </>
+    
+      ));
+    
+}
+export default Display;
