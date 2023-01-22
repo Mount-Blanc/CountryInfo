1fde1a811207c15800a7e626264632dc9e3db10e
Mount-Blanc <mountblanc87@gmail.com>
2023-01-22 15:25:18 -0800

    added video background

--- Front-end/nationinfo/src/App.js
+++ Front-end/nationinfo/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Display from './Display';
+import Earth from './assets/Earth.mp4'
 
 function App() {
   return (
     <div >
-      <Display/>
+     <video src={Earth} autoPlay loop/>
     </div>
   );
 }
