33496eca400382a94103bee83b3bf2d8e415aa8b
Mount-Blanc <mountblanc87@gmail.com>
2023-01-22 10:33:33 -0800

    Deleted backend, too many bugs

--- Front-end/countryinfo/src/App.js
+++ Front-end/countryinfo/src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
-import {useState} from 'react'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
-import { useQuery, gql } from '@apollo/client';
 
-const client = new ApolloClient({
+function App() {
+
+  const client = new ApolloClient({
 
     uri: 'http://localhost:4000',
   
     cache: new InMemoryCache(),
   
   });
+  const author =client
 
-function App() {
+  .query({
 
-  
-  const [userinput, setuserinput] = useState('')
+    query: gql`
+
+      query GetLocations {
+
+        books {
+
+        author
+
+        }
+
+      }
+
+    `,
+
+  })
+
+  .then((result) => console.log(result));
   return (
-    <ApolloProvider client={client}>
-      <input value={(event)=> {setuserinput(event.target.value)}}type='text'placeholder='Enter Country'/>
-    </ApolloProvider>
+    <div >
+     {author}
+    </div>
   );
 }
 
