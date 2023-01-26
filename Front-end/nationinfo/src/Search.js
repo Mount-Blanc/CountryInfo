389a3e25ee731feae5b2727d41fe3c62b30f3877
Mount-Blanc <mountblanc87@gmail.com>
2023-01-26 10:41:15 -0800

    got the select drop down list working

--- Front-end/nationinfo/src/Search.js
+++ Front-end/nationinfo/src/Search.js
@@ -3,26 +3,46 @@ import { useState } from 'react';
 import Display from './Display';
 import { useContext } from 'react';
 import { Context } from './context';
+import { useQuery, gql } from '@apollo/client';
+
+
+
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
+
+const list = ['america','germany']
 
 function Search () {
 const ctx = useContext(Context)
+// const { loading, error, data } = useQuery(LIST_COUNTRIES)
 
-    const [userinput, setuserinput] = useState('')
-    const [isSubmitted, setisSubmitted] = useState(false)
+const [userinput, setuserinput] = useState(list[0])
+const [isSubmitted, setisSubmitted] = useState(false)
 
     const selectHandler = (event) => {
         setuserinput(event.target.value)
+        console.log(userinput)
     }
 
    
-
     return(
         <>
        <select
       value={userinput}
-      onChange={selectHandler}
-    >
-        <option>{"america"}</option>
+      onChange={selectHandler}>
+        {list.map((option, index) => (
+        <option key={index} value={option}>{option}</option>
+
+        ))}
+
+
+
     </select>
      {isSubmitted && <Display/> }
     </>
