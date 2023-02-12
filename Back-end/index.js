86a19d352167895b39d7130e93e70cde0b0d0b1b
Mount-Blanc <mountblanc87@gmail.com>
2023-02-12 12:43:28 -0600

    added get method

--- Back-end/index.js
+++ Back-end/index.js
@@ -51,6 +51,17 @@ await server.start();
 
 // and our expressMiddleware function.
 
+app.get('/', function(req,res) {
+  res.sendFile(
+    path.join(__dirname, "../Front-end/build/index.html"),
+   function(err){
+    if (err) {
+      res.status(500).send(err)
+    }}
+  )
+})
+
+
 app.use(
 
   '/graphql',
