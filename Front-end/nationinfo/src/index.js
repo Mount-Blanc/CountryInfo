e041904d970441db3eb35ea08f0119413e858e50
Mount-Blanc <mountblanc87@gmail.com>
2023-02-13 13:41:01 -0600

    changed local host to loopback ip

--- Front-end/nationinfo/src/index.js
+++ Front-end/nationinfo/src/index.js
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 
 const client = new ApolloClient({
 
-  uri: 'http://localhost:4000/graphql',
+  uri: 'http://127.0.0.1:4000/graphql',
 
   cache: new InMemoryCache(),
 
