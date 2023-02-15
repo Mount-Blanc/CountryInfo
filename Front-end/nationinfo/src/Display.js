be240b9e6c808d8788e9161e74bceb391d254169
Mount-Blanc <mountblanc87@gmail.com>
2023-02-15 15:29:15 -0600

    formatted code

--- Front-end/nationinfo/src/Display.js
+++ Front-end/nationinfo/src/Display.js
@@ -1,55 +1,52 @@
-import { useQuery, gql } from '@apollo/client';
-import { useContext } from 'react';
-import { Context } from './context';
-import Card from './Card';
-import './Display.css'
+import { useQuery, gql } from "@apollo/client";
+import { useContext } from "react";
+import { Context } from "./context";
+import Card from "./Card";
+import "./Display.css";
 const GET_COUNTRY = gql`
-
-query GET_COUNTRY ($userCountry: String!) {
-  Country(userCountry: $userCountry) {
-    name {
-      common
-      official
+  query GET_COUNTRY($userCountry: String!) {
+    Country(userCountry: $userCountry) {
+      name {
+        common
+        official
+      }
+      flag
+      population
+      capital
+      continents
     }
-    flag
-    population
-    capital
-    continents
   }
-}`;
+`;
 
+const Display = ({ userinput }) => {
+  const ctx = useContext(Context);
+  //  const userinput =ctx.countryValue
 
-const Display = ({userinput}) => {
-     const ctx=useContext(Context)
-    //  const userinput =ctx.countryValue
+  const { loading, error, data } = useQuery(GET_COUNTRY, {
+    variables: { userCountry: `${userinput}` },
+  });
 
-  const { loading, error, data } = useQuery(GET_COUNTRY,
-      {variables: {userCountry:`${userinput}`}} )
+  let content;
+  if (loading) {
+    return (content = <p>Loading...</p>);
+  }
+  // if (error) {
+  //   return content = <p>An error has occured {error.status}!</p>
+  // }
 
-  let content
-if (loading) {
-return content = <p>Loading...</p>
-}
-// if (error) {
-//   return content = <p>An error has occured {error.status}!</p>
-// }
-
-    return data.Country.map(({name,flag,capital,continents,population}) => (
-        
-          <Card key={name.official} className='display'>
+  return data.Country.map(({ name, flag, capital, continents, population }) => (
+    <Card key={name.official} className="display">
       <div>
-          <h3>{flag}{name.common}</h3> 
-        </div>
-        <div>Official Name:{name.official}</div>
-        <div>Capital:{capital === null ?<p>N/A</p> :capital[0]}</div>  
-        <div>Continent:{continents}</div>  
-        <div>Population:{population}</div>  
-
-
-         </Card>
-      
-    
-      ));
-    
-}
+        <h3>
+          {flag}
+          {name.common}
+        </h3>
+      </div>
+      <div>Official Name:{name.official}</div>
+      <div>Capital:{capital === null ? <p>N/A</p> : capital[0]}</div>
+      <div>Continent:{continents}</div>
+      <div>Population:{population}</div>
+    </Card>
+  ));
+};
 export default Display;
--- Front-end/nationinfo/src/Search.js
+++ Front-end/nationinfo/src/Search.js
@@ -1,59 +1,54 @@
-import './Search.css'
-import { useState } from 'react';
-import Display from './Display';
-import { useContext } from 'react';
-import { Context } from './context';
-import { useQuery, gql } from '@apollo/client';
-
-
+import "./Search.css";
+import { useState } from "react";
+import Display from "./Display";
+import { useContext } from "react";
+import { Context } from "./context";
+import { useQuery, gql } from "@apollo/client";
 
 const LIST_COUNTRIES = gql`
-query List_COUNTRIES {
-  Select {
-    common
-    official
-    
+  query List_COUNTRIES {
+    Select {
+      common
+      official
+    }
   }
-}
 `;
 
+function Search() {
+  const ctx = useContext(Context);
 
-function Search () {
-const ctx = useContext(Context)
+  const { data, loading, error } = useQuery(LIST_COUNTRIES);
+  // {
 
-   const { data, loading, error } = useQuery(LIST_COUNTRIES)
-    // {
+  //   fetchPolicy: 'network-only', // Used for first execution
 
-    //   fetchPolicy: 'network-only', // Used for first execution
-    
-    //   nextFetchPolicy: 'cache-first', // Used for subsequent executions
-    
-    // });
-const [userinput, setuserinput] = useState('United States')
-const [isSubmitted, setisSubmitted] = useState(false)
+  //   nextFetchPolicy: 'cache-first', // Used for subsequent executions
 
-    const selectHandler = (event) => {
-        setuserinput(event.target.value)
-    }
+  // });
+  const [userinput, setuserinput] = useState("United States");
+  const [isSubmitted, setisSubmitted] = useState(false);
 
-if (loading) {
-  return <p>Loading...</p>
-}
+  const selectHandler = (event) => {
+    setuserinput(event.target.value);
+  };
 
-    return(
-      <>
-    <div className='search'>
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <div className="search">
         <select value={userinput} onChange={selectHandler}>
-           {data.Select.map( ({common,official}) => (
-         <option key={official} value={common}>
-         {common}
-        </option>
-    ))
-    } 
-      </select>      
-       </div>
-     <Display userinput={userinput}/> </>
-    )
-  
+          {data.Select.map(({ common, official }) => (
+            <option key={official} value={common}>
+              {common}
+            </option>
+          ))}
+        </select>
+      </div>
+      <Display userinput={userinput} />{" "}
+    </>
+  );
 }
-export default Search
+export default Search;
